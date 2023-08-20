d9d316c219466d7e1294ab5cf566928d7aad3c2c
kokoro <128305501+dev090613@users.noreply.github.com>
2023-08-20 23:09:37 +0900

    comments app

--- articles/admin.py
+++ articles/admin.py
@@ -1,10 +1,20 @@
 # articles/admin.py
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
 
-# To see more information in the admin about article
+# class CommentInline(admin.StackedInline):  # To switch to Stackedinline
+class CommentInline(admin.TabularInline):
+    model = Comment
+    # To change the default number of extra field
+    extra = 0
+
+
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+    # stackedinline의 경우 list_display를 각주처리
     list_display = [
         "title",
         "body",
@@ -12,6 +22,17 @@ class ArticleAdmin(admin.ModelAdmin):
     ]
 
 
+# # To see more information in the admin about article
+# class ArticleAdmin(admin.ModelAdmin):
+#     list_display = [
+#         "title",
+#         "body",
+#         "author",
+#     ]
+
+
 admin.site.register(Article, ArticleAdmin)
 # LEGACY, without ArticleAdmin class
 # admin.site.register(Article)
+
+admin.site.register(Comment)
